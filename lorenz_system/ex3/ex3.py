9eda1689ef2a939bf2264d1fc018b6e3f55eb31b
José Scott <62150664+jescott07@users.noreply.github.com>
2022-11-23 15:12:43 -0300

    Add files via upload

--- lorenz_system/ex3/ex3.py
+++ lorenz_system/ex3/ex3.py
@@ -2,23 +2,21 @@ from ex3 import lorenz as lr
 
 from matplotlib import pyplot as plt
 
-
-
 lr.r = 10
 lr.tf = 100
 
 lr.rk4()
 
-
 x1a,x2a,x3a = lr.x[:,0],lr.x[:,1],lr.x[:,2]
 
-plt.figure()
-plt.title('Lorenz System: ' r'$y_1 \times y_2$ ' 'for r = 10')
-plt.xlabel(r'$y_1$')
-plt.ylabel(r'$y_2$')
-plt.plot(x1a,x2a)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.set_title('Lorenz System: for r = 30')
+ax.set_xlabel('x1')
+ax.set_ylabel('x2')
+ax.set_zlabel('x3')
+ax.plot(x1a, x2a, x3a)
 plt.savefig('ls_1.png',dpi=300)
-plt.show()
 plt.close()
 
 lr.r = 30
@@ -28,11 +26,12 @@ lr.rk4()
 
 x1b,x2b,x3b = lr.x[:,0],lr.x[:,1],lr.x[:,2]
 
-plt.figure()
-plt.title('Lorenz System: ' r'$y_1 \times y_2$ ' 'for r = 30')
-plt.xlabel(r'$y_1$')
-plt.ylabel(r'$y_2$')
-plt.plot(x1b,x2b)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.set_title('Lorenz System: for r = 30')
+ax.set_xlabel('x1')
+ax.set_ylabel('x2')
+ax.set_zlabel('x3')
+ax.plot(x1b, x2b, x3b)
 plt.savefig('ls_2.png',dpi=300)
-plt.show()
 plt.close()
